0c43c0f2f4a0d5bbf11d8b23c786ad5f94f78d78
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-10-18 16:33:26 -0500

    Ejemplo de asincronia con callback

--- async/callbacks.js
+++ async/callbacks.js
@@ -1,13 +1,14 @@
-const soyAsync = () => {
-    console.log('hola, soy una función asíncrona');
+const soyAsync = miCallback => {
+    console.log('Hola, soy una función asíncrona');
     setTimeout(() => {
         console.log('Estoy siendo asíncrona');
+        miCallback()
     }, 1000)
 }
 
 console.log('Iniciando proceso');
-soyAsync();
-console.log('Terminando proceso');
-//La jerarquía de los logs no importa al momento de ejecución debido a que se presenta de forma asincrónica
-//Se imprime el log de inicio, luego el de la fn asíncrona, después el de fin, y cuando pase el segundo se imprime el log de 'estoy siendo asíncrona'
+soyAsync(() => {
+    console.log('Terminando proceso');
+});
+
 
