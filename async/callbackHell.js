9371e7bca05b6e9f02e2fb6589dec573be8a43a4
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-10-24 22:15:27 -0500

    Resolución de callback hell aplicando la recursividad y funciones más legibles

--- async/callbackHell.js
+++ async/callbackHell.js
@@ -19,16 +19,34 @@ const adios = (nombre, otroCallback) => {
     }, 1000)
 }
 
+const conversacion = (nombre, veces, callback) => {
+    if(veces > 0){
+        hablar(() => {
+            conversacion(nombre, --veces, callback) //Aplicamos una funcion recursiva, la llamamos para que se ejecute la cantidad de veces que indiquemos
+        }) //cada que se ejecute conversación, a las veces se le retará 1, por ello aplicamos las condiciones
+    } else { //cuando sea igual a 0, ejecutamos adios, y recibe como callback un console log
+        adios(nombre, callback);
+    }
+}
+
 console.log('Iniciando proceso');
 
 hola('Adri', (nombre) => {
-    hablar(() => {
-        hablar(() => {
-            hablar(() => {
-                adios(nombre, () => {
-                    console.log('Terminando proceso');
-                })
-            })
-        })
-    })
-})
+   conversacion(nombre, 3, () => {
+       console.log('Terminamos proceso');
+   });
+});
+
+// hola('Adri', (nombre) => {
+//     hablar(() => {
+//         hablar(() => {
+//             hablar(() => {
+//                 adios(nombre, () => {
+//                     console.log('Terminando proceso');
+//                 })
+//             })
+//         })
+//     })
+// })
+
+//Con funciones más legibles y con más lógica y complejidad, podemos evitar el callback hell
