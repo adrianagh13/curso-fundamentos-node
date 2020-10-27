5567da263bf226b35d355ec205e6177d603c8b86
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-10-27 16:42:26 -0600

    Ejemplo de child-process con spawn

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,12 +1,18 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 
-exec('node modulos/consola.js ', (err, stdout, sterr) => {
-    if (err) {
-        console.error(err);
-        return false;
-    }
-    console.log(stdout);
+let proceso = spawn('cmd.exe', ['/c', 'dir']);
+
+console.log(proceso.pid);
+
+proceso.stdout.on('data', function (dato) { //con un standard out imprimimos la data de nuestro proceso
+    console.log(dato.toString()); //los convertimos a string porque spawn recibe binarios
+});
+
+proceso.on('exit', () => {
+    console.log('El proceso terminó');
+    console.log('Está muerto el proceso?');
+    console.log(proceso.killed); //retorna un booleano para saber si se ha matado el proceso
 })
 
-//Ejemplo en que un proceso hijo ejecuta un proceso de node por debajo
-//Con exec podemos ejecutar procesos sencillos por debajo del proceso hijo
+//Mediante spawn podemos iniciar procesos que devuelven grandes cantidades de datos binarios
+//Se debe usar spawn cuando se quieren obtener datos desde que el proceso arranca
