fd3a40c877c16abb210316bec73648a8d6924556
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-10-27 18:46:37 -0600

    Creación de cabecera y escritura como response

--- modulos/http.js
+++ modulos/http.js
@@ -5,8 +5,16 @@ const http = require('http');
 http.createServer((req, res) => {
     console.log('Nueva peticion');
     console.log(req.url);
-    
+
+    //Escribir una cabecera(se pueden visulizar en consola)
+    res.writeHead(201, { 'Content-Type': 'text/plain' }) //método de cabecera, código de edo, y cabecera
+
+    //Escribir respuesta al usuario
+    res.write('hola, ya se usar http de node')
+
     res.end();//terminar la peticion
 }).listen(3002) //el servidor escucha al puerto 3002
 
 console.log('Escuchando al puerto 3002');
+
+//Puedes visualizar las cabeceras en el inspector-Network-Código de estado
