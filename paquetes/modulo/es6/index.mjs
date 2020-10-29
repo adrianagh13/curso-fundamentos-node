84bc0aa3d32dd893bc55746eb2c37452d8e09630
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-10-28 22:37:02 -0600

    Modulo manejado con es6

---
+++ paquetes/modulo/es6/index.mjs
@@ -0,0 +1,7 @@
+import modulo from './modulo.mjs';
+
+console.log(modulo.prop1);
+modulo.saludo();
+
+//Extension .mjs, se utiliza para sistemas de modulos, unicamente de manera experimental
+//Para ejecutar módulos experimantales: node --experimental-modules dirección 
---
+++ paquetes/modulo/es6/modulo.mjs
@@ -0,0 +1,8 @@
+const saludo = () => {
+    console.log('Hola, este es mi primer módulo creado');
+}
+
+export default {
+    saludo,
+    prop1: 'Hola mundo',
+};
