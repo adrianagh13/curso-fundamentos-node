d77d5c5fadb35fb3147e1c0ed1badd1c8707dd0f
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-11-10 19:34:02 -0600

    error first

--- trucos/errorFirst.js
+++ trucos/errorFirst.js
@@ -9,18 +9,19 @@ const asincrona = (callback) => {
     })
 }
 
-try {
-    asincrona((err, dato) => {
-        if (err) {
-            // console.error('Tenemos un error');
-            // console.error(err);
-            // return false;
-            throw err;
-        }
-    
-        console.log('Todo va bien, la data es: ', dato); //tambien se puede concatenar con , 
-    });
-} catch (error) {
-    console.error('Hemos capturado un error')   
-    console.error(error)
-}
+
+asincrona((err, dato) => {
+    if (err) {
+        console.error('Tenemos un error');
+        console.error(err);
+        return false;
+        // throw err;
+    }
+
+    console.log('Todo va bien, la data es: ', dato); //tambien se puede concatenar con , 
+});
+
+
+//ERROR FIRST CALLBACKS --> Se utilizan para pasar primero el error y los datos posteriormente
+//Entonces se puede verificar el primer argumento que es el error, para manejarlo si es que hubiera
+//En caso de que no haya ningun error se pueden utilizar los argumentos siguientes y seguir adelante
